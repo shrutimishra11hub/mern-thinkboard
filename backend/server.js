18e5d3a8eec74bc3e7a1e30db2f700f454642955
SHRUTIMISHRA <mshruti952@gmail.com>
2026-02-24 10:29:50 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://mern-thinkboard-khaki.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
