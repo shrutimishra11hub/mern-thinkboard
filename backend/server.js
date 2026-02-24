b8f6bf0a176833c59d94c63f231cee67330fbdc5
SHRUTIMISHRA <mshruti952@gmail.com>
2026-02-24 10:06:51 +0530

    Remove frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -20,16 +20,8 @@ app.use("/api/auth", authRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
-    );
-  });
-}
-
+  
 
 
 const PORT = process.env.PORT || 5001;
