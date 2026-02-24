de5d5c133667a5cca47fcc93770da4228c3cbf71
SHRUTIMISHRA <mshruti952@gmail.com>
2026-02-24 10:02:27 +0530

    Fix render port issue

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,39 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
 
+const app = express();
+
 connectDB();
 
-const app = express();   // ✅ FIRST create app
-
-app.use(cors());         // ✅ Then use middleware
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 app.use("/api/auth", authRoutes);
-connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
+  });
+}
 
 
-app.listen(5001, () => {
-  console.log("Server started on PORT: 5001");
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
 });
-
